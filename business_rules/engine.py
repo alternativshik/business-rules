7ab079c4816339150a4fd121cd0e5fa83e731eed
Serhii Maltsev <alternativshik@gmail.com>
2019-07-09 13:52:03 +0300

    clear unused code && update python clear python2

--- business_rules/engine.py
+++ business_rules/engine.py
@@ -1,24 +1,20 @@
 from .fields import FIELD_NO_INPUT
 
-def run_all(rule_list,
-            defined_variables,
-            defined_actions,
-            stop_on_first_trigger=False):
 
+def run_all(rule_list, defined_variables, stop_on_first_trigger=False):
     rule_was_triggered = False
     for rule in rule_list:
-        result = run(rule, defined_variables, defined_actions)
+        result = run(rule, defined_variables)
         if result:
             rule_was_triggered = True
             if stop_on_first_trigger:
                 return True
     return rule_was_triggered
 
-def run(rule, defined_variables, defined_actions):
-    conditions, actions = rule['conditions'], rule['actions']
-    rule_triggered = check_conditions_recursively(conditions, defined_variables)
+
+def run(rule, defined_variables):
+    rule_triggered = check_conditions_recursively(rule, defined_variables)
     if rule_triggered:
-        do_actions(actions, defined_actions)
         return True
     return False
 
@@ -45,6 +41,7 @@ def check_conditions_recursively(conditions, defined_variables):
         assert not ('any' in keys or 'all' in keys)
         return check_condition(conditions, defined_variables)
 
+
 def check_condition(condition, defined_variables):
     """ Checks a single rule condition - the condition will be made up of
     variables, values, and the comparison operator. The defined_variables
@@ -54,6 +51,7 @@ def check_condition(condition, defined_variables):
     operator_type = _get_variable_value(defined_variables, name)
     return _do_operator_comparison(operator_type, op, value)
 
+
 def _get_variable_value(defined_variables, name):
     """ Call the function provided on the defined_variables object with the
     given name (raise exception if that doesn't exist) and casts it to the
@@ -61,12 +59,16 @@ def _get_variable_value(defined_variables, name):
 
     Returns an instance of operators.BaseType
     """
+
     def fallback(*args, **kwargs):
-        raise AssertionError("Variable {0} is not defined in class {1}".format(
-                name, defined_variables.__class__.__name__))
+        raise AssertionError(
+            "Variable {0} is not defined in class {1}".format(name, defined_variables.__class__.__name__)
+        )
+
     method = getattr(defined_variables, name, fallback)
     val = method()
     return method.field_type(val)
+
 
 def _do_operator_comparison(operator_type, operator_name, comparison_value):
     """ Finds the method on the given operator_type and compares it to the
@@ -76,21 +78,13 @@ def _do_operator_comparison(operator_type, operator_name, comparison_value):
     comparison_value is whatever python type to compare to
     returns a bool
     """
+
     def fallback(*args, **kwargs):
-        raise AssertionError("Operator {0} does not exist for type {1}".format(
-            operator_name, operator_type.__class__.__name__))
+        raise AssertionError(
+            "Operator {0} does not exist for type {1}".format(operator_name, operator_type.__class__.__name__)
+        )
+
     method = getattr(operator_type, operator_name, fallback)
     if getattr(method, 'input_type', '') == FIELD_NO_INPUT:
         return method()
     return method(comparison_value)
-
-
-def do_actions(actions, defined_actions):
-    for action in actions:
-        method_name = action['name']
-        def fallback(*args, **kwargs):
-            raise AssertionError("Action {0} is not defined in class {1}"\
-                    .format(method_name, defined_actions.__class__.__name__))
-        params = action.get('params') or {}
-        method = getattr(defined_actions, method_name, fallback)
-        method(**params)
